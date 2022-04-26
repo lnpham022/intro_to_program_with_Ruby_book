0b4cfd05d32ac157b395756f1699878cbeb9b535
Lina Pham <lnpham022@gmail.com>
2022-04-26 13:52:17 -0700

    Update the_basics/exercise_07.rb

--- the_basics/exercise_07.rb
+++ the_basics/exercise_07.rb
@@ -3,5 +3,7 @@
 SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
   from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
 
-  # There is an opening bracket somewhere in the program without a 
+=begin
+There is an opening bracket somewhere in the program without a 
 closing bracket following it. It may have happened when creating a hash.
+=end
