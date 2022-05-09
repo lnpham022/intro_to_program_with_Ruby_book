0657104f6ca8dfe44b6d2b69be611a12ebbdf574
Lina Pham <lnpham022@gmail.com>
2022-05-09 12:38:17 -0700

    Add methods exercises

---
+++ methods/exercise_04.rb
@@ -0,0 +1,11 @@
+=begin
+What will the following code print to the screen?
+=end
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
---
+++ methods/exercise_06.rb
@@ -0,0 +1,13 @@
+=begin
+What does the following error message tell you?
+=end
+
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in 'calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in '<main>'
+
+=begin
+You are calling a method called calculate_product 
+that requires two arguments, but you are only providing one
+=end
